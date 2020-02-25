61eb5611f383143fb229e965a28d070f92a0192d
OldDriver9527 <1187557502@qq.com>
2020-02-25 19:49:11 +0800

    docker compose

---
+++ src/main/org/olddriver/DockerCompose.java
@@ -0,0 +1,22 @@
+package org.olddriver;
+
+public class DockerCompose {
+    /*
+    *
+    * Docker Compose
+    * 定义，运行多个docker容器的工具，使用python编写
+    * Docker Compose通过一个独立的docker-compose.yml配置文件，将一组关联的服务定义为一个项目
+    * 服务指一个docker容器
+    * 项目指由一组关联docker容器构成的完整业务单元
+    * docker compose默认管理项目
+    *
+    * 安装Docker Compose
+    * 1.下载可执行二进制文件
+    * curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    * 2.为文件添加执行权限
+    * chmod +x /usr/local/bin/docker-compose
+    *
+    *
+    *
+     */
+}
