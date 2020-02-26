21c62a9dcb74084000e07cae8c10a3a948be19a2
OldDriver9527 <1187557502@qq.com>
2020-02-26 19:26:10 +0800

    paas

---
+++ src/main/org/olddriver/DevelopmentPlatform.java
@@ -0,0 +1,15 @@
+package org.olddriver;
+
+public class DevelopmentPlatform {
+    /*
+    *
+    * GitLab
+    * git仓库管理平台
+    *
+    * Nexus
+    * maven仓库管理平台
+    *
+    * Registry
+    * docker仓库管理平台
+     */
+}
